36b1683915486836124e9b993c7c4ec6a609769b
zhengmianwei <zhengmianwei@163.com>
2018-03-10 17:37:48 +0800

    提交项目测试git

--- src/com/Test.java
+++ src/com/Test.java
@@ -5,6 +5,6 @@ package com;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.printf("test-----------");
+        System.out.printf("test---------");
     }
 }
