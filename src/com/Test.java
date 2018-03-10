ef7d3fa78a8d0ac8f05017e57dfc2de5e28cc490
zhengmianwei <zhengmianwei@163.com>
2018-03-10 18:06:13 +0800

    提交GitTest

---
+++ src/com/Test.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by Administrator on 2018/3/10.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.printf("测试----------");
+    }
+}
