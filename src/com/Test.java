2f2d0ee27d68038e93e5db80a50379c27da55c73
zhengmianwei <zhengmianwei@163.com>
2018-03-10 17:48:25 +0800

    提交Test

--- src/com/Test.java
+++ src/com/Test.java
@@ -5,7 +5,7 @@ package com;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.printf("test---------");
+        System.out.printf("test-------");
     }
 }
 
